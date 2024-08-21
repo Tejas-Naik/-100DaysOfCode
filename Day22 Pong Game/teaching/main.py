f1645609e6e89c0251e86e8ecf5b596afa7f6d43
Tejas-Naik <rntejas2005@gmail.com>
2024-08-21 14:44:52 +0530

    Created a Paddle and listening to the keypresses

--- Day22 Pong Game/teaching/main.py
+++ Day22 Pong Game/teaching/main.py
@@ -4,5 +4,32 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800,height=600)
 screen.title("Pong")
+screen.tracer(0)
+
+paddle = Turtle()
+paddle.color("white")
+paddle.shape("square")
+paddle.shapesize(stretch_wid=5,stretch_len=1)
+paddle.penup()
+paddle.goto(350,0)
+
+# Listening to the keypress
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(),new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(),new_y)
+
+
+screen.listen()
+screen.onkey(go_up,"Up")
+screen.onkey(go_down,"Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
 
 screen.exitonclick()
