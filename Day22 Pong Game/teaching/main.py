c83fda627cd31e33cf42a73b5fc658a5f5ee2154
Tejas-Naik <rntejas2005@gmail.com>
2024-08-21 14:59:28 +0530

    Created second paddle

--- Day22 Pong Game/teaching/main.py
+++ Day22 Pong Game/teaching/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle,Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
@@ -6,26 +7,14 @@ screen.setup(width=800,height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.color("white")
-paddle.shape("square")
-paddle.shapesize(stretch_wid=5,stretch_len=1)
-paddle.penup()
-paddle.goto(350,0)
-
-# Listening to the keypress
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(),new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(),new_y)
-
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
 
 screen.listen()
-screen.onkey(go_up,"Up")
-screen.onkey(go_down,"Down")
+screen.onkey(r_paddle.go_up,"Up")
+screen.onkey(r_paddle.go_down,"Down")
+screen.onkey(l_paddle.go_up,"w")
+screen.onkey(l_paddle.go_down,"s")
 
 game_is_on = True
 while game_is_on:
